154cbd2ee4daa910ba6a0375e4a77f6a7223ab0e
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-19 12:47:00 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,40 @@
 #include "Pokedex.h"
 
+using namespace std;
+
+// function prototypes
+void menu(const Pokedex& p);
+
 int main(int argc, char** argv) {
     Pokedex p(argv[1]);
-
     p.read_dataset();
+
+    menu(p);
     return 0;
+}
+
+void menu(const Pokedex& p) {
+    std::cout << "Welcome to your pokedex!" << std::endl;
+
+    int option = -1;
+    while (option != 0) {
+        std::cout << "Please choose an option:\n" << std::endl;
+        std::cout << "1) Search Pokemon" << std::endl;
+        std::cout << "2) Search Ability" << std::endl;
+        std::cout << "0) Exit" << std::endl;
+        std::cout << "Option: ";
+        cin >> option;
+
+        if (option == 1) {
+            std::cout << "Search by Pokemon" << std::endl;
+        } else if (option == 2) {
+            std::cout << "Search Ability" << std::endl;
+        } else if (option == 0) {
+            break;
+        } else {
+            std::cout << "Invalid option. Try again." << std::endl;
+        }
+    }
+
+    std::cout << "Goodbye!" << std::endl;
 }
