f19e7660a30a25c9ae9ea817871a781cd65f12ec
Giancarlos Dominguez <Giancarlosd@smu.edu>
2023-06-19 23:29:22 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 // function prototypes
-void menu(const Pokedex& p);
+void menu(Pokedex& p);
 
 int main(int argc, char** argv) {
     Pokedex p(argv[1]);
@@ -13,12 +13,12 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-void menu(const Pokedex& p) {
+void menu(Pokedex& p) {
     std::cout << "Welcome to your pokedex!" << std::endl;
 
     int option = -1;
     while (option != 0) {
-        std::cout << "Please choose an option:\n" << std::endl;
+        std::cout << "Please choose an option:" << std::endl;
         std::cout << "1) Search Pokemon" << std::endl;
         std::cout << "2) Search Ability" << std::endl;
         std::cout << "0) Exit" << std::endl;
@@ -26,13 +26,18 @@ void menu(const Pokedex& p) {
         cin >> option;
 
         if (option == 1) {
-            std::cout << "Search by Pokemon" << std::endl;
+            cin.ignore();
+            std::string name;
+            std::cout << "\nEnter Pokemon Name: ";
+
+            getline(cin, name);
+            p.search_pokemon(name);
         } else if (option == 2) {
-            std::cout << "Search Ability" << std::endl;
+            std::cout << "Search Ability\n" << std::endl;
         } else if (option == 0) {
             break;
         } else {
-            std::cout << "Invalid option. Try again." << std::endl;
+            std::cout << "Invalid option. Try again.\n" << std::endl;
         }
     }
 
